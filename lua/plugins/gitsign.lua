59d5aeb816343d62cd7ec4927892320d00463c44
parsa.poorsistani <parsa.poorsistani@cafebazaar.ir>
2025-01-13 19:13:40 +0330

    gitsing bug fixed

--- lua/plugins/gitsign.lua
+++ lua/plugins/gitsign.lua
@@ -2,7 +2,15 @@ return {
   'lewis6991/gitsigns.nvim',
   lazy = false,
   config = function()
-    require('gitsigns').setup()
+    require('gitsigns').setup({
+      current_line_blame = true,  -- This enables inline blame information
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol',  -- Place blame text at end of line
+        delay = 500,            -- Delay before blame information appears (in milliseconds)
+        ignore_whitespace = false,
+      },
+      current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+    })
   end
 }
-

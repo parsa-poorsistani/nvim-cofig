7cf4fe909f4741ea6adf3afb815fa6a6928a2f9b
parsa.poorsistani <parsa.poorsistani@cafebazaar.ir>
2026-09-11 03:53:55 +0330

    fold/comment coloring

--- lua/plugins/oxocarbon.lua
+++ lua/plugins/oxocarbon.lua
@@ -1,13 +1,21 @@
 return {
   "nyoom-engineering/oxocarbon.nvim",
-  lazy = false,  -- We set this to true since we don't need it loaded immediately
+  lazy = false,
   priority = 1000,  -- Setting this higher than rose-pine to ensure it can override when selected
   config = function()
-    -- This function will be called when the plugin is loaded
-    vim.opt.background = "dark"  -- You can adjust this to "light" if you prefer
+    vim.opt.background = "dark"
     vim.cmd.colorscheme("oxocarbon")
+    vim.api.nvim_set_hl(0, "Folded", {
+        fg = "#f5ca45",
+        bg = "#484848",
+    })
+
+    vim.api.nvim_set_hl(0, "Comment", {
+      fg = "#4d8734",
+      italic = true,
+    })
     -- vim.cmd("colorscheme oxocarbon")
-    -- If you want transparency, uncomment these lines:
+    -- Transparency, uncomment these lines:
     -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
